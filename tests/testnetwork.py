b578b8c96de38d4e6dfeebeadc90f4511e376c77
Devin Sevilla <devin@ning.com>
2011-04-26 11:39:59 -0700

    Network test was missing fields parameter

--- tests/testnetwork.py
+++ tests/testnetwork.py
@@ -5,16 +5,30 @@ import basetest
 class TestNetworks(basetest.BaseTestCase):
 
     def test_get_networks(self):
-        fields = []
+        fields = ["id",
+                "author",
+                "createdDate",
+                "subdomain",
+                "name",
+                "iconUrl",
+                "defaultUserIconUrl",
+                "blogPostModeration",
+                "eventModeration",
+                "groupModeration",
+                "photoModeration",
+                "userModeration",
+                "videoModeration",
+                "xapiStatus",
+                ]
         attrs = self.field_args(fields)
         attrs['count'] = 2
         content = self.api.get("Network/alpha", attrs)
-        self.assertTrue(content["success"])
+        self.assertTrue(content['success'])
 
 
 def suite():
     return unittest.TestLoader().loadTestsFromTestCase(TestNetworks)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
