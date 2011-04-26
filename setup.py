38086f4e9bbfee26c27f9a5da59144d64905fe70
Devin Sevilla <devin@ning.com>
2011-04-26 11:00:36 -0700

    Added support for setuptools

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ningapi",
+    version="1.0.1",
+    author="Ning Inc.",
+    author_email="help@ning.com",
+    maintainer='Devin Sevilla',
+    url="https://github.com/ning/ning-api-python",
+    description=("Python Client for accessing the Ning API"),
+    license="Apache License, Version 2.0",
+    install_requires=['oauth2'],
+    packages=['ningapi', 'tests'],
+    test_suite='tests',
+    zip_safe=True,
+    classifiers=[
+              'License :: OSI Approved :: Apache Software License',
+              'Programming Language :: Python :: 2.6',
+              'Programming Language :: Python :: 2.7'
+              ],
+)
